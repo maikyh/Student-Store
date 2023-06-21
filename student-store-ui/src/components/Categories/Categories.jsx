3dcc6975b36656e18287c26561e7241417e33cf2
Miguel Garza Carranza <maiky@meta.com>
2023-06-21 13:34:15 -0700

    Finished All Categories section

--- student-store-ui/src/components/Categories/Categories.jsx
+++ student-store-ui/src/components/Categories/Categories.jsx
@@ -1,10 +1,35 @@
-import * as React from "react"
-import "./Categories.css"
+import React, { useState } from "react";
+import "./Categories.css";
 
 export default function Categories() {
+  const [selectedCategory, setSelectedCategory] = useState("All Categories");
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const categories = ["All Categories", "Clothing", "Food", "Accessories", "Tech"];
+
   return (
-    <div className="mx-auto categories">
-      Hello2
+    <div className="container">
+      <div className="row justify-content-center mt-4 mb-4">
+        {categories.map((category, index) => (
+          <div className="col-auto" key={index}>
+            <button
+              className={`btn btn-outline-success me-3 ${selectedCategory === category ? "active" : ""}`}
+              onClick={() => handleCategoryClick(category)}
+            >
+              {category}
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className="row mb-4">
+        <div className="col text-center">
+          <h3>{selectedCategory}</h3>
+          {/* Add your content for the selected category */}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- student-store-ui/src/components/ProductGrid/ProductGrid.jsx
+++ student-store-ui/src/components/ProductGrid/ProductGrid.jsx
@@ -9,9 +9,6 @@ export default function ProductGrid() {
   console.log(products);
   return (
     <div className="mx-auto product-grid">
-      <div className="text-left mt-10 mb-20 text-uppercase font-semibold text-xl">
-        <h3 className="fw-bold">Best Selling Products</h3>
-      </div>
       <div className="row">
         {products?.map((product) => (
           <div className="col-md-3" >
