f8016e81d2f64a9ff00ffae67008875bf3169332
Miguel Garza Carranza <maiky@meta.com>
2023-06-21 17:24:09 -0700

    Implemented the fetch of the API /store endpoint

--- student-store-ui/src/components/ProductGrid/ProductGrid.jsx
+++ student-store-ui/src/components/ProductGrid/ProductGrid.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "../ProductCard/ProductCard";
-import dataProducts from "../../../../student-store-express-api/data/db.json";
 import "./ProductGrid.css";
 
-const products = dataProducts.products;
+const url = `https://codepath-store-api.herokuapp.com/store`;
 
 export default function ProductGrid({ selectedCategory }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   let currentProducts;
-  if(selectedCategory !== "All Categories"){
-     currentProducts = products.filter(item => item.category === selectedCategory);
-  }
-  else{
+  if (selectedCategory !== "All Categories") {
+    currentProducts = products.filter((item) => item.category === selectedCategory);
+  } else {
     currentProducts = products;
   }
 
@@ -19,8 +32,8 @@ export default function ProductGrid({ selectedCategory }) {
     <div className="mx-auto product-grid">
       <div className="row">
         {currentProducts?.map((product) => (
-          <div className="col-md-3" >
-            <ProductCard product={product} key={product.id} />
+          <div className="col-md-3" key={product.id}>
+            <ProductCard product={product} />
           </div>
         ))}
       </div>
