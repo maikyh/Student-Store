db4f36587a2154e4db72fc7ec96417018f525873
Miguel Garza Carranza <maiky@meta.com>
2023-06-26 22:11:25 -0700

    Added standard app.js for express

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,12 @@
-// YOUR CODE HERE
+const express = require("express")
+const morgan = require("morgan")
+const app = express()
+
+app.use(morgan("tiny"))
+app.use(express.json())
+
+app.get("/", async (req, res) => {
+    res.json({ ping1: "pong2" })
+})
+
+module.exports = app
